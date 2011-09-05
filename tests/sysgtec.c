ad4676cd1566b97fd585afc26d2a3cb1258d81d9
Nicolas Bourdaud <nicolas.bourdaud@epfl.ch>
2011-09-05 15:47:46 +0200

    gtec: tests multiple system and simplify the test code

--- tests/sysgtec.c
+++ tests/sysgtec.c
@@ -24,29 +24,27 @@
 #include <unistd.h>
 #include <errno.h>
 #include <eegdev.h>
+#include "fakelibs/fakegtec.h"
 
 #define DURATION	4	// in seconds
 #define NSAMPLE	4
-#define NEEG	16
-#define NEXG	0
-#define NTRI	1
 #define scaled_t	float
 
-struct grpconf grp[3] = {
+static struct grpconf grp[3] = {
 	{
 		.sensortype = EGD_EEG,
 		.index = 0,
 		.iarray = 0,
 		.arr_offset = 0,
-		.nch = NEEG,
+		.nch = 16,
 		.datatype = EGD_FLOAT
 	},
 	{
 		.sensortype = EGD_SENSOR,
 		.index = 0,
 		.iarray = 0,
-		.arr_offset = NEEG*sizeof(float),
-		.nch = NEXG,
+		.arr_offset = 16*sizeof(float),
+		.nch = 0,
 		.datatype = EGD_FLOAT
 	},
 	{
@@ -54,10 +52,148 @@ struct grpconf grp[3] = {
 		.index = 0,
 		.iarray = 1,
 		.arr_offset = 0,
-		.nch = NTRI,
+		.nch = 1,
 		.datatype = EGD_INT32
 	}
 };
+
+static
+int check_signals_f(size_t ns, const float* sig, const int32_t* tri)
+{
+	size_t i=0;
+	int nchtri = grp[2].nch;
+	int nch = grp[0].nch + grp[1].nch;
+	int ich;
+	float expval;
+	int32_t exptri;
+	int retval = 0;
+
+	static int checking = 0;
+	static int nstot = 0, nsread = 0;
+
+
+	// Identify the beginning of the expected sequence
+	if (!checking) {
+		for (i=0; i<ns; i++)
+			if (tri[i*nchtri] == get_trigger_val(0, 0)) {
+				checking = 1;
+				break;
+			}
+		if (!checking && nsread > 113) {
+			fprintf(stderr, "\tCannot find the beginning of the sequence\n");
+			retval = -1;
+		}
+	}
+
+	for (; i<ns; i++) {
+		// Verify the values in the analog channels
+		for (ich=0; ich<nch; ich++) {
+			expval = get_analog_val(nstot, ich);
+			if (sig[i*nch+ich] != expval) {
+				fprintf(stderr, "\tSignal value (%f) different from the one expected (%f) at sample %zu ch:%u\n", sig[i*nch+ich], expval, i+nsread, ich);
+				retval = -1;
+			}
+		}
+		
+		// Verify the values in the trigger channels
+		for (ich=0; ich<nchtri; ich++) {
+			exptri = get_trigger_val(nstot, ich);
+			if (tri[i*nchtri+ich] != exptri) {
+				fprintf(stderr, "\tTrigger value (%i) different from the one expected (%i) at sample %zu ch:%u\n", tri[i*nchtri+ich], exptri, i+nsread, ich);
+				retval = -1;
+			}
+		}
+		nstot++;
+	}
+
+
+	nsread += ns;
+	return retval;
+}
+
+
+int check_signals_d(size_t ns, const double* sig, const int32_t* tri)
+{
+	size_t i=0;
+	int nchtri = grp[2].nch;
+	int nch = grp[0].nch + grp[1].nch;
+	int ich;
+	double expval;
+	int32_t exptri;
+	int retval = 0;
+
+	static int checking = 0;
+	static int nstot = 0, nsread = 0;
+
+
+	// Identify the beginning of the expected sequence
+	if (!checking) {
+		for (i=0; i<ns; i++)
+			if (tri[i*nchtri] == get_trigger_val(0, 0)) {
+				checking = 1;
+				break;
+			}
+		if (!checking && nsread > 113) {
+			fprintf(stderr, "\tCannot find the beginning of the sequence\n");
+			retval = -1;
+		}
+	}
+
+	for (; i<ns; i++) {
+		// Verify the values in the analog channels
+		for (ich=0; ich<nch; ich++) {
+			expval = get_analog_val(nstot, ich);
+			if (sig[i*nch+ich] != expval) {
+				fprintf(stderr, "\tSignal value (%f) different from the one expected (%f) at sample %zu ch:%u\n", sig[i*nch+ich], expval, i+nsread, ich);
+				retval = -1;
+			}
+		}
+		
+		// Verify the values in the trigger channels
+		for (ich=0; ich<nchtri; ich++) {
+			exptri = get_trigger_val(nstot, ich);
+			if (tri[i*nchtri+ich] != exptri) {
+				fprintf(stderr, "\tTrigger value (%i) different from the one expected (%i) at sample %zu ch:%u\n", tri[i*nchtri+ich], exptri, i+nsread, ich);
+				retval = -1;
+			}
+		}
+		nstot++;
+	}
+
+
+	nsread += ns;
+	return retval;
+}
+
+
+static
+int simple_trigger_check(int is, int ns, int ntri, const int32_t* tri_t)
+{
+	static int32_t triref[8];
+	static int refinit = 0;
+
+	int j, k, retcode = 0;
+	
+	if (is == 0) {
+		memcpy(triref, tri_t, ntri*sizeof(*tri_t));
+		refinit = 1;
+	}
+
+	for (j=0; j<ns; j++) {
+		for (k=0; k<ntri; k++) {
+			if (tri_t[ntri*j+k] != triref[k])  {
+				fprintf(stderr, "\ttrigger value (0x%08x) "
+				  "different from the one expected (0x%08x)"
+				  " at sample %i channel %i\n",
+				  tri_t[ntri*j+k], triref[k], is+j, k);
+				triref[k] = tri_t[ntri*j+k];
+				retcode = -1;
+			}
+		}
+	}
+
+	return retcode;
+}
 
 
 static
@@ -73,8 +209,8 @@ int print_cap(struct eegdev* dev)
 	eeg_nmax = egd_get_numch(dev, EGD_EEG);
 	sensor_nmax = egd_get_numch(dev, EGD_SENSOR);
 	trigger_nmax = egd_get_numch(dev, EGD_TRIGGER);
-	egd_channel_info(dev, EGD_EEG, 0, 
-	                 EGD_PREFILTERING, prefiltering, EGD_EOL);
+	egd_channel_info(dev, EGD_EEG, 0,
+				EGD_PREFILTERING, prefiltering, EGD_EOL);
 	
 	printf("\tsystem capabilities:\n"
 	       "\t\tdevice type: %s\n"
@@ -90,7 +226,7 @@ int print_cap(struct eegdev* dev)
 	return (int)sampling_freq;
 }
 
-
+static
 int test_chinfo(struct eegdev* dev)
 {
 	unsigned int i, eegnch, sensnch, trinch;
@@ -131,23 +267,51 @@ int test_chinfo(struct eegdev* dev)
 }
 
 
-int read_eegsignal(void)
+static
+struct eegdev* open_device(unsigned int nsystem, struct grpconf group[3])
 {
 	struct eegdev* dev;
-	size_t strides[2] = {
-		NEEG*sizeof(scaled_t)+NEXG*sizeof(scaled_t),
-		NTRI*sizeof(int32_t)
-	};
-	scaled_t *eeg_t;
-	int32_t *tri_t, triref;
-	int i, j, retcode = 1;
-	int fs;
+	unsigned int i;
+	int type = grp[0].datatype;
+	size_t tsize = (type == EGD_FLOAT ? sizeof(float) : sizeof(double));
+	char devicestr[256] = "gtec|deviceid|any";
 
-	eeg_t = calloc(NSAMPLE*(NEEG+NEXG),sizeof(*eeg_t));
-	tri_t = calloc(NSAMPLE*NTRI,sizeof(*tri_t));
+	for (i=1; i<nsystem; i++)
+		strcat(devicestr, "+any");
 
-	if ( !(dev = egd_open("gtec")) )
+	if (!(dev = egd_open(devicestr)))
+		return NULL;
+
+	group[0].nch = egd_get_numch(dev, EGD_EEG);
+	group[1].arr_offset = group[0].nch * tsize;
+	group[1].nch = egd_get_numch(dev, EGD_SENSOR);
+	group[2].nch = egd_get_numch(dev, EGD_TRIGGER);
+
+	return dev;
+}
+
+
+static
+int read_eegsignal(unsigned int nsystem, int bsigcheck)
+{
+	struct eegdev* dev;
+	int type = grp[0].datatype;
+	size_t strides[2];
+	void *eeg_t;
+	int32_t *tri_t;
+	int ntri, fs, i, baddata, retcode = 1;
+	size_t tsize = (type == EGD_FLOAT ? sizeof(float) : sizeof(double));
+
+	if (!(dev = open_device(nsystem, grp)))
 		goto exit;
+
+	// Get number of channels and configure structures
+	strides[0] = (grp[0].nch + grp[1].nch)*tsize;
+	strides[1] = grp[2].nch*sizeof(int32_t);
+	ntri = grp[2].nch;
+
+	eeg_t = calloc(strides[0], NSAMPLE);
+	tri_t = calloc(strides[1], NSAMPLE);
 
 	fs = print_cap(dev);
 
@@ -163,23 +327,26 @@ int read_eegsignal(void)
 	if (egd_start(dev))
 		goto exit;
 	
-	i = 0;
-	while (i < fs*DURATION) {
+	for (i=0; i < fs*DURATION; i += NSAMPLE) {
 		if (egd_get_data(dev, NSAMPLE, eeg_t, tri_t) < 0) {
 			fprintf(stderr, "\tAcq failed at sample %i\n",i);
 			goto exit;
 		}
 
-		if (i == 0)
-			triref = tri_t[0];
-
-		for (j=0; j<NSAMPLE; j++)
-			if (tri_t[j] != triref)  {
-				fprintf(stderr, "\ttrigger value (0x%08x) different from the one expected (0x%08x) at sample %i\n", tri_t[j], triref, i+j);
-				triref = tri_t[j];
+		// No checking
+		if (!bsigcheck) {
+			if (simple_trigger_check(i, NSAMPLE, ntri, tri_t))
 				retcode = 2;
-			}
-		i += NSAMPLE;
+			continue;
+		}
+
+		if (type == EGD_FLOAT)
+			baddata = check_signals_f(NSAMPLE, eeg_t, tri_t);
+		else
+			baddata = check_signals_d(NSAMPLE, eeg_t, tri_t);
+
+		if (baddata)
+			retcode = 2;
 	}
 
 	if (egd_stop(dev))
@@ -205,18 +372,44 @@ exit:
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-	int retcode = 0;
+	int retcode = 0, opt;
+	unsigned int nsystem = 1;
+	int bsigcheck = 0, usedouble = 0;
 
-	fprintf(stderr, "\tTesting gtec\n\tVersion : %s\n", egd_get_string());
+	while ((opt = getopt(argc, argv, "n:c:d:")) != -1) {
+		switch (opt) {
+		case 'n':
+			nsystem = atoi(optarg);
+			break;
+
+		case 'c':
+			bsigcheck = atoi(optarg);
+			break;
+
+		case 'd':
+			usedouble = atoi(optarg);
+			break;
+
+		default:	/* '?' */
+			fprintf(stderr, "Usage: %s [-n numsystem] "
+			                "[-c checking_expected_signals] "
+					"[-d use_double]\n",
+				argv[0]);
+			return EXIT_FAILURE;
+		}
+	}
+
+	fprintf(stderr, "\tTesting gtec with %u system(s) with %s data type\n\tVersion : %s\n",
+		nsystem, usedouble ? "double" : "float", egd_get_string());
+
+	if (usedouble)
+		grp[0].datatype = grp[1].datatype = EGD_DOUBLE;
 
 	// Test generation of a file
-	retcode = read_eegsignal();
+	retcode = read_eegsignal(nsystem, bsigcheck);
 
 
 	return retcode;
 }
-
 
 
--- tests/testfakegtec.sh
+++ tests/testfakegtec.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
-LD_PRELOAD=$builddir/fakelibs/.libs/libfakegtec.so testfakegtec$EXEEXT
+export LD_PRELOAD=$builddir/fakelibs/.libs/libfakegtec.so
+prg=sysgtec$EXEEXT
+
+retval=0
+
+if ! $prg -n 1 -d 0 -c 1
+then
+	retval=1
+fi
+
+if ! $prg -n 1 -d 1 -c 1
+then
+	retval=1
+fi
+
+if ! $prg -n 2 -d 0 -c 1
+then
+	retval=1
+fi
+
+if ! $prg -n 2 -d 1 -c 1
+then
+	retval=1
+fi
+
+exit $retval
