db03f0cd407e7c39d01128c608e74cdadae23cf7
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2012-03-21 23:55:07 +0100

    Use autogen.sh to import the gnulib package

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,48 @@
 #!/bin/sh -e
 
-cd `dirname $0`
+glmodules="accept bind byteswap clock-functions close connect dlopen getaddrinfo lib-symbol-visibility listen netdb regex setsockopt shutdown socket"
+gloptions="--local-dir=gnulib-local --lgpl --libtool"
 
-if ! gnulib-tool --update > gnulib.log ; then
+# Get absolute path of current and package path
+execpath=$(pwd)
+cd $(dirname $0)
+packagepath=$(pwd)
+cd $execpath
+
+
+# Get gnulib-tool path from cache
+GL_CMD_CACHE=".gnulib-tool-cmd.cache"
+if [ -f "$GL_CMD_CACHE" ] ; then
+	GLTOOL=`cat $GL_CMD_CACHE`
+else
+	GLTOOL=gnulib-tool
+fi
+
+
+# Process options (output filename)
+while getopts "g:" option
+do
+  case $option in
+    g) cd $(dirname $OPTARG)
+       GLTOOL="$(pwd)/$(basename $OPTARG)"
+       cd $execpath
+       ;;
+    *) exit 1 ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+
+# run gnulib-tool --update from the package folder
+cd $packagepath
+if ! $GLTOOL  $gloptions --import $glmodules> gnulib.log ; then
 	cat gnulib.log
 	rm gnulib.log
 	exit 1
 fi
 rm gnulib.log
+echo "$GLTOOL" > $GL_CMD_CACHE
 
-autoreconf -i
+
+autoreconf -fi
 
