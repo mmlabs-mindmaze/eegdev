20dbe20a9aef416af34407adb40d1b9033c9d53f
Gabriel Ganne <gabriel.ganne@mindmaze.com>
2020-01-16 13:46:25 +0100

    win32 tests - add path to libeegdev.dll
    Change-Id: I92e35aeb1834247613aa3ff1a51e916c89b0cdd6

--- doc/examples/testexamples.sh
+++ doc/examples/testexamples.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-if ! eegdev_acq$EXEEXT -d saw > eegdev_acq.log
+set -e
+set -x
+
+export PATH="$EEGDEV_PLUGINS_DIR:$CORELIB_PATH:$PATH"
+
+if ! $(dirname $0)/eegdev_acq$EXEEXT -d saw > eegdev_acq.log
 then
 	exit 1
 fi
