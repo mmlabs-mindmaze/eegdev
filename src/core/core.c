bb452ed6d10d9ac8197b8d3034fa09605811d342
Gabriel Ganne <gabriel.ganne@mindmaze.com>
2020-01-16 11:04:41 +0100

    core - forbid sensor init without channel
    Change-Id: I61e4efae7809b8b54c73045f077df7d3a12dc97b

--- src/core/core.c
+++ src/core/core.c
@@ -473,7 +473,7 @@ int egdi_set_cap(struct devmodule* mdev, const struct plugincap* cap)
 		auxlabel += len;
 	}
 
-	if (find_supported_sensor(dev, dev->cap.nch, dev->cap.chmap))
+	if (!nch || find_supported_sensor(dev, dev->cap.nch, dev->cap.chmap))
 		return -1;
 
 	return 0;
